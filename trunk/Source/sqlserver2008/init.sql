50d49692af5d5aa2654d39cc3bc5346fd21c4151
farseerfc@gmail.com <farseerfc@gmail.com@75712bf9-698a-4f0c-68d8-d99044246f15>
2010-12-21 18:23:26 +0000

    add event listener for Register form

---
+++ trunk/Source/sqlserver2008/init.sql
@@ -0,0 +1,7 @@
+insert into [UserGroup](groupId,groupName) values(0,'管理员');
+insert into [UserGroup](groupId,groupName) values(1,'基督徒');
+insert into [UserGroup](groupId,groupName) values(2,'了解中');
+insert into [UserGroup](groupId,groupName) values(3,'非基督徒');
+
+insert into [UserInfo]([groupId],[username],[password],[email]) values(0,'farseer','angela','farseerfc@gmail.com');
+insert into [UserInfo]([groupId],[username],[password],[email]) values(1,'angela','farseer','angela.toughitout@gmail.com');
