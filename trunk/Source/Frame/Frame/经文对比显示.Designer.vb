37f412e01f160e1c3963df743bc3016142a85378
farseerfc@gmail.com <farseerfc@gmail.com@75712bf9-698a-4f0c-68d8-d99044246f15>
2010-12-26 11:38:33 +0000

    完成“经文对比显示”内部控件的衔接。 待解决：希腊版的字符集显示问题。

--- trunk/Source/Frame/Frame/经文对比显示.Designer.vb
+++ trunk/Source/Frame/Frame/经文对比显示.Designer.vb
@@ -43,12 +43,17 @@ Partial Class 经文对比显示
         Me.ComboBox2 = New System.Windows.Forms.ComboBox
         Me.Label2 = New System.Windows.Forms.Label
         Me.Label3 = New System.Windows.Forms.Label
+        Me.nudChapter = New System.Windows.Forms.NumericUpDown
+        Me.nudVerse = New System.Windows.Forms.NumericUpDown
+        CType(Me.nudChapter, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nudVerse, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'RichTextBox1
         '
         Me.RichTextBox1.Location = New System.Drawing.Point(3, 114)
         Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
         Me.RichTextBox1.Size = New System.Drawing.Size(319, 391)
         Me.RichTextBox1.TabIndex = 0
         Me.RichTextBox1.Text = ""
@@ -67,7 +72,7 @@ Partial Class 经文对比显示
         'B_ChapterUp
         '
         Me.B_ChapterUp.Image = CType(resources.GetObject("B_ChapterUp.Image"), System.Drawing.Image)
-        Me.B_ChapterUp.Location = New System.Drawing.Point(391, 42)
+        Me.B_ChapterUp.Location = New System.Drawing.Point(580, 5)
         Me.B_ChapterUp.Name = "B_ChapterUp"
         Me.B_ChapterUp.Size = New System.Drawing.Size(33, 33)
         Me.B_ChapterUp.TabIndex = 1
@@ -77,7 +82,7 @@ Partial Class 经文对比显示
         'B_ChapterDown
         '
         Me.B_ChapterDown.Image = CType(resources.GetObject("B_ChapterDown.Image"), System.Drawing.Image)
-        Me.B_ChapterDown.Location = New System.Drawing.Point(458, 41)
+        Me.B_ChapterDown.Location = New System.Drawing.Point(647, 4)
         Me.B_ChapterDown.Name = "B_ChapterDown"
         Me.B_ChapterDown.Size = New System.Drawing.Size(33, 33)
         Me.B_ChapterDown.TabIndex = 8
@@ -87,7 +92,7 @@ Partial Class 经文对比显示
         'B_VerseUp
         '
         Me.B_VerseUp.Image = CType(resources.GetObject("B_VerseUp.Image"), System.Drawing.Image)
-        Me.B_VerseUp.Location = New System.Drawing.Point(391, 78)
+        Me.B_VerseUp.Location = New System.Drawing.Point(580, 41)
         Me.B_VerseUp.Name = "B_VerseUp"
         Me.B_VerseUp.Size = New System.Drawing.Size(33, 33)
         Me.B_VerseUp.TabIndex = 9
@@ -97,7 +102,7 @@ Partial Class 经文对比显示
         'B_VerseDown
         '
         Me.B_VerseDown.Image = CType(resources.GetObject("B_VerseDown.Image"), System.Drawing.Image)
-        Me.B_VerseDown.Location = New System.Drawing.Point(458, 80)
+        Me.B_VerseDown.Location = New System.Drawing.Point(647, 43)
         Me.B_VerseDown.Name = "B_VerseDown"
         Me.B_VerseDown.Size = New System.Drawing.Size(33, 33)
         Me.B_VerseDown.TabIndex = 10
@@ -166,6 +171,7 @@ Partial Class 经文对比显示
         '
         Me.RichTextBox2.Location = New System.Drawing.Point(379, 115)
         Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.ReadOnly = True
         Me.RichTextBox2.Size = New System.Drawing.Size(319, 391)
         Me.RichTextBox2.TabIndex = 16
         Me.RichTextBox2.Text = ""
@@ -186,15 +192,15 @@ Partial Class 经文对比显示
         'Combo1
         '
         Me.Combo1.BackColor = System.Drawing.Color.SkyBlue
+        Me.Combo1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.Combo1.Font = New System.Drawing.Font("华文行楷", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
         Me.Combo1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.Combo1.FormattingEnabled = True
         Me.Combo1.Items.AddRange(New Object() {"创世纪 (Genesis)", "出埃及记 (Exodus)", "利未记 (Leviticus)", "民数记 (Numbers)", "申命记 (Deuteronomy)", "约书亚记 (Joshua)", "士师记 (Judges)", "路得记 (Ruth)", "撒母耳记上(1 Samuel)", "撒母耳记下(2 Samuel)", "列王纪上(1 Kings)", "列王纪下(2 Kings)", "历代志上(1 Chronicles)", "历代志下(2 Chronicles)", "以斯拉记(Ezra)", "尼希米记(Nehemiah)", "以斯帖记 (Esther)", "约伯记 (Job)", "诗篇 (Psalms)", "箴言 (Proverbs)", "传道书 (Ecclesiastes)", "雅歌 (Song of Songs)", "以赛亚书 (Isaiah)", "耶利米书 (Jeremiahr)", "耶利米哀歌(Lamentations)", "以西结书 (Ezekielk)", "何西阿书 (Hosea)", "约珥书 (Joel)", "阿摩司书 (Amoss)", "俄巴底亚书 (Obadiah)", "约拿书 (Jonah)", "弥迦书 (Micah)", "那鸿书 (Nahum)", "哈巴谷书 (Habakkuk)", "西番雅书 (Zephaniah)", "哈该书 (Haggai)", "撒迦利亚书 (Zechariah)", "玛拉基书 (Malachi)", "马太福音 (Matthew)", "马可福音 (Mark)", "路加福音 (Luke)", "约翰福音 (John)", "使徒行传 (Acts)", "罗马书 (Romans, Rom)", "哥林多前书 (1 Corinthians)", "哥林多后书 (2 Corinthians)", "加拉太书 (Galatians)", "以弗所书 (Ephesus)", "腓立比书 (Philippians)", "歌罗西书 (Colossians)", "帖撒罗尼迦前书 (1 Thessalonians)", "帖撒罗尼迦后书 (2 Thessalonians)", "提摩太前书 (1 Timothy)", "提摩太后书 (2 Timothy)", "提多书 (Titus)", "腓利门书 (Philemon)", "希伯来书 (Hebrews) ", "雅各书 (James)", "彼得前书(1 Peter)", "彼得后书(2 Peter)", "约翰一书(1 John)", "约翰二书(2 John)", "约翰三书(3 John)", "犹大书 (Jude)", "启示录 (Revelation)"})
-        Me.Combo1.Location = New System.Drawing.Point(207, 63)
+        Me.Combo1.Location = New System.Drawing.Point(182, 61)
         Me.Combo1.Name = "Combo1"
         Me.Combo1.Size = New System.Drawing.Size(180, 29)
         Me.Combo1.TabIndex = 19
-        Me.Combo1.Text = "请选择书卷"
         '
         'Button1
         '
@@ -214,7 +220,7 @@ Partial Class 经文对比显示
         Me.ComboBox2.ForeColor = System.Drawing.Color.White
         Me.ComboBox2.FormattingEnabled = True
         Me.ComboBox2.Items.AddRange(New Object() {"中文和合本", "德语版", "希伯来原文", "ESV", "KJV", "ISV", "BWE", "WEB", "JST"})
-        Me.ComboBox2.Location = New System.Drawing.Point(519, 79)
+        Me.ComboBox2.Location = New System.Drawing.Point(526, 79)
         Me.ComboBox2.Name = "ComboBox2"
         Me.ComboBox2.Size = New System.Drawing.Size(172, 29)
         Me.ComboBox2.TabIndex = 21
@@ -225,7 +231,7 @@ Partial Class 经文对比显示
         Me.Label2.BackColor = System.Drawing.Color.SkyBlue
         Me.Label2.Font = New System.Drawing.Font("幼圆", 16.0!)
         Me.Label2.ForeColor = System.Drawing.Color.Black
-        Me.Label2.Location = New System.Drawing.Point(425, 46)
+        Me.Label2.Location = New System.Drawing.Point(490, 48)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(32, 22)
         Me.Label2.TabIndex = 22
@@ -237,11 +243,25 @@ Partial Class 经文对比显示
         Me.Label3.BackColor = System.Drawing.Color.SkyBlue
         Me.Label3.Font = New System.Drawing.Font("幼圆", 16.0!)
         Me.Label3.ForeColor = System.Drawing.Color.Black
-        Me.Label3.Location = New System.Drawing.Point(425, 80)
+        Me.Label3.Location = New System.Drawing.Point(490, 79)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(32, 22)
         Me.Label3.TabIndex = 23
         Me.Label3.Text = "节"
+        '
+        'nudChapter
+        '
+        Me.nudChapter.Location = New System.Drawing.Point(379, 49)
+        Me.nudChapter.Name = "nudChapter"
+        Me.nudChapter.Size = New System.Drawing.Size(105, 21)
+        Me.nudChapter.TabIndex = 24
+        '
+        'nudVerse
+        '
+        Me.nudVerse.Location = New System.Drawing.Point(379, 80)
+        Me.nudVerse.Name = "nudVerse"
+        Me.nudVerse.Size = New System.Drawing.Size(105, 21)
+        Me.nudVerse.TabIndex = 25
         '
         '经文对比显示
         '
@@ -249,6 +269,8 @@ Partial Class 经文对比显示
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(703, 514)
+        Me.Controls.Add(Me.nudVerse)
+        Me.Controls.Add(Me.nudChapter)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.ComboBox2)
@@ -268,6 +290,8 @@ Partial Class 经文对比显示
         Me.Controls.Add(Me.RichTextBox1)
         Me.Name = "经文对比显示"
         Me.Text = "经文对比显示"
+        CType(Me.nudChapter, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nudVerse, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -291,4 +315,6 @@ Partial Class 经文对比显示
     Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents nudChapter As System.Windows.Forms.NumericUpDown
+    Friend WithEvents nudVerse As System.Windows.Forms.NumericUpDown
 End Class
--- trunk/Source/Frame/Frame/经文查询显示.vb
+++ trunk/Source/Frame/Frame/经文查询显示.vb
@@ -1,28 +1,39 @@
 ﻿Public Class 经文查询显示
 
     Private Sub 经文查询显示_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        loadVersionList()
-        loadBookList()
-    End Sub
-
-    Private Sub loadVersionList()
         ComboBox1.Items.Clear()
         ComboBox1.Items.AddRange(Version.GetAllVersions().ToArray())
-    End Sub
 
-    Private Sub loadBookList()
+
         Dim books As List(Of Book) = Book.GetAllBooks()
 
         Combo1.Items.Clear()
         Combo1.Items.AddRange(books.ToArray())
 
+        reloadText()
     End Sub
+
 
     Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
         Dim ver As Version = DirectCast(ComboBox1.SelectedItem, Version)
 
+        Dim lastSelect As String
+        If Combo1.SelectedItem Is Nothing Then
+            lastSelect = ""
+        Else
+            lastSelect = DirectCast(Combo1.SelectedItem, Book).book
+        End If
+
         Combo1.Items.Clear()
         Combo1.Items.AddRange(BookVersion.GetBooksByVersion(ver.initial).ToArray())
+
+        For i As Integer = 0 To Combo1.Items.Count - 1
+            If DirectCast(Combo1.Items.Item(i), Book).book.Equals(lastSelect) Then
+                Combo1.SelectedIndex = i
+            End If
+        Next
+
+        reloadText()
 
     End Sub
 
@@ -33,6 +44,8 @@
         Dim nrChapter As Integer = CInt(Key.GetChapterCount(book.book, ver.initial))
         nudChapter.Maximum = nrChapter
         nudChapter.Minimum = 1
+
+        reloadText()
     End Sub
 
     Private Sub nudChapter_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nudChapter.ValueChanged
@@ -44,17 +57,27 @@
         nudVerse.Maximum = nrVerse
         nudVerse.Minimum = 1
 
+
+        reloadText()
     End Sub
 
     Private Sub nudVerse_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nudVerse.ValueChanged
+        reloadText()
+
+    End Sub
+
+    Private Sub reloadText()
         Dim ver As Version = DirectCast(ComboBox1.SelectedItem, Version)
         Dim book As Book = DirectCast(Combo1.SelectedItem, Book)
         Dim chapter As Integer = CInt(nudChapter.Value)
         Dim verse As Integer = CInt(nudVerse.Value)
 
+        If ver Is Nothing Or book Is Nothing Or chapter = 0 Or verse = 0 Then
+            Return
+        End If
+
         Dim osisId As String = Key.GetOsisId(book.book, chapter, verse)
         Dim textContent As String = Module1.Text.GetText(ver.initial, osisId)
         RichTextBox1.Text = textContent
-
     End Sub
 End Class
