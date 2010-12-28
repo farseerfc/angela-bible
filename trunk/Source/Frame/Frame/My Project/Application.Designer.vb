ac211cba0d0e7e0a24a467ad2dfe5da0d3f3dd5d
angela.toughitout@gmail.com <angela.toughitout@gmail.com@75712bf9-698a-4f0c-68d8-d99044246f15>
2010-12-28 02:27:10 +0000

    圣经版本的章节里加入了关于圣经的基本信息。。改了一些控件的名字。米了

--- trunk/Source/Frame/Frame/My Project/Application.Designer.vb
+++ trunk/Source/Frame/Frame/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApplication1.意见反馈
+            Me.MainForm = Global.WindowsApplication1.圣经版本简介
         End Sub
     End Class
 End Namespace
