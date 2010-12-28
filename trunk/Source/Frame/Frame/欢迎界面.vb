3fe1a116c9b2bf1a5e08adbc310c0a277f8e354d
angela.toughitout@gmail.com <angela.toughitout@gmail.com@75712bf9-698a-4f0c-68d8-d99044246f15>
2010-12-28 04:04:32 +0000

    nothing

--- trunk/Source/Frame/Frame/欢迎界面.vb
+++ trunk/Source/Frame/Frame/欢迎界面.vb
@@ -6,11 +6,6 @@ Public Class 欢迎界面
     Private Sub 欢迎界面_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Me.AxShockwaveFlash1.Movie = Application.StartupPath & "\开机动画.swf"
     End Sub
-
-    Private Sub AxShockwaveFlash1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AxShockwaveFlash1.Enter
-
-    End Sub
-
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
         If (Me.Visible) Then
             fstart = New 登录界面
