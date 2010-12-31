7b61e395e92fafa282841a019067a579a6573a98
angela.toughitout@gmail.com <angela.toughitout@gmail.com@75712bf9-698a-4f0c-68d8-d99044246f15>
2010-12-31 15:46:22 +0000

    终于。。。完成了么。。。

--- trunk/Source/Frame/Frame/My Project/Application.Designer.vb
+++ trunk/Source/Frame/Frame/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApplication1.登录界面
+            Me.MainForm = Global.WindowsApplication1.管理员查询界面
         End Sub
     End Class
 End Namespace
--- trunk/Source/Frame/Frame/意见反馈.vb
+++ trunk/Source/Frame/Frame/意见反馈.vb
@@ -5,7 +5,10 @@
         Dim timestamp As Date = Date.Now
         Dim username As String = loginedUser.Username
 
-
+        If TextBox1.Text = "" Then
+            MsgBox("请先输入您宝贵的意见！", MsgBoxStyle.OkOnly)
+Ins:        Return
+        End If
         Using sqlConnection As SqlClient.SqlConnection = New SqlClient.SqlConnection(strConnect)
             sqlConnection.Open()
             sqlConnection.CreateCommand()
@@ -18,33 +21,22 @@
             Dim rowcount As Integer = sqlCommand.ExecuteNonQuery()
             If rowcount > 0 Then
                 '添加成功
-                MsgBox("您的意见已经告知管理员，谢谢您的参与！")
+                MsgBox("您的意见已经告知管理员，谢谢您的帮助！")
             Else
                 '添加失败
                 MsgBox("数据库链接失败！")
             End If
-
-
         End Using
     End Sub
 
-    Private Sub DateTimePicker1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DateTimePicker1.ValueChanged
+    Private Sub DateTimePicker1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Dim dateTimePicker1 As New DateTimePicker()
-
-        ' Set the MinDate and MaxDate.因为只能显示当天
-        dateTimePicker1.MinDate = DateTime.Today
-        dateTimePicker1.MaxDate = DateTime.Today
-        dateTimePicker1.ShowCheckBox = True
-        dateTimePicker1.ShowUpDown = True
-
-
     End Sub
 
     Private Sub bhome_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bhome.Click
         fhome = New 主页
         fhome.Show()
         Me.Close()
-
     End Sub
 
     Private Sub bexit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bexit.Click
